351847d5a0a9de473b3c362575220866fb1e0831
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-07 11:19:49 +0530

    Angular 17 application - AppComponent - Sample html with css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-spa';
+  appTitle:string = 'Welcome to Angular development'
 }
